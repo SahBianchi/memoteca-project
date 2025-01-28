07f583981bd308bd03eaaffc36a5119b6146f709
Samantha Bianchi <samantha.bianchi@onerpm.com>
2025-01-28 17:45:01 -0300

    feat: added search field

--- src/app/components/thoughts/list-thoughts/list-thoughts.component.ts
+++ src/app/components/thoughts/list-thoughts/list-thoughts.component.ts
@@ -5,6 +5,7 @@ import { Pensamento } from '../thought-interface';
 import { ThoughtComponent } from "../thought/thought.component";
 import { ThoughtService } from '../thought.service';
 import { BtnLoadMoreComponent } from "./btn-load-more/btn-load-more.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-list-thoughts',
@@ -13,7 +14,8 @@ import { BtnLoadMoreComponent } from "./btn-load-more/btn-load-more.component";
     RouterModule,
     CommonModule,
     ThoughtComponent,
-    BtnLoadMoreComponent
+    BtnLoadMoreComponent,
+    FormsModule
 ],
   templateUrl: './list-thoughts.component.html',
   styleUrl: './list-thoughts.component.css'
@@ -22,23 +24,33 @@ export class ListThoughtsComponent {
   listaPensamentos: Pensamento[] = [];
   paginaAtual: number = 1;
   haMaisPensamentos: boolean = true;
+  filtro: string = '';
 
   constructor(
     private service: ThoughtService
   ) {}
 
   ngOnInit(): void {
-    this.service.listar(this.paginaAtual).subscribe((listaPensamentos) => {
+    this.service.listar(this.paginaAtual, this.filtro).subscribe((listaPensamentos) => {
       this.listaPensamentos = listaPensamentos;
     });
   }
 
   carregarMaisPensamentos(){
-    this.service.listar(++this.paginaAtual).subscribe(listaPensamentos => {
+    this.service.listar(++this.paginaAtual, this.filtro).subscribe(listaPensamentos => {
       this.listaPensamentos.push(...listaPensamentos);
       if(!listaPensamentos.length){
         this.haMaisPensamentos = false;
       }
     })
+  }
+
+  pesquisarPensamentos(){
+    this.haMaisPensamentos = true;
+    this.paginaAtual = 1;
+    this.service.listar(this.paginaAtual, this.filtro)
+      .subscribe(listaPensamentos => {
+        this.listaPensamentos = listaPensamentos;
+      })
   }
 }
